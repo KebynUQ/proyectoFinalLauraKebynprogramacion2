abd99e313df319437c53cc2a2296f74f843cc27b
KebynUQ <kebynj.ochoap@uqvirtual.edu.co>
2026-05-21 10:29:34 -0500

    Add model classes for Administrador and enums for EstadoAsiento, EstadoCompra, and EstadoEntrada

---
+++ src/main/java/uniquindio/edu/co/model/enums/EstadoCompra.java
@@ -0,0 +1,12 @@
+package uniquindio.edu.co.eventos.model.enums;
+
+public enum EstadoCompra {
+    CREADA,
+    PENDIENTE_PAGO,
+    PENDIENTE_CONFIRMACION,
+    PAGADA,
+    CONFIRMADA,
+    CANCELADA,
+    REEMBOLSADA,
+    NOVEDAD
+}
